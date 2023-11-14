5e4bb72c3040c4720641082e87566ca8ea1dfdbf
Novi Adi Triswandi <icambing@gmail.com>
2023-11-14 13:58:15 +0700

    Add dockerfile and push image

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Menggunakan image Node.js versi 14.21-alpine sebagai base image.
+FROM node:14.21-alpine as builder
+
+# Menentukan direktori kerja di dalam container sebagai /app.
+WORKDIR /app
+
+# Menyalin file package.json dan package-lock.json ke dalam direktori kerja.
+COPY package*.json ./
+
+# Mengonfigurasi npm untuk tidak memeriksa SSL agar dapat mengakses registry npm.
+RUN npm config set strict-ssl false
+
+# Menginstall dependencies yang dibutuhkan berdasarkan package.json.
+RUN npm ci
+
+# Menyalin semua file dengan ekstensi .js ke dalam direktori kerja.
+COPY ./*.js ./
+
+# Mengekspos port 3000 untuk aplikasi yang berjalan di dalam container.
+EXPOSE 3000
+
+# Menentukan perintah default yang akan dijalankan saat container berjalan.
+CMD [ "node", "index.js" ]
---
+++ build_push_image_order_service.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Perintah untuk memeriksa apakah Docker sudah terinstal
+if command -v docker &> /dev/null; then
+    
+    # Jika Docker terinstalasi, jalankan perintah docker build
+    docker build -t novi-adi/order-service:latest .
+
+    # Melihat daftar image di lokal
+    docker images
+
+    # Nama paket Docker yang akan digunakan di GitHub Packages
+    DOCKER_PACKAGE="ghcr.io/novi-adi/order-service:latest"
+
+    # Mengubah nama image agar sesuai dengan format GitHub Packages
+    docker tag novi-adi/order-service:latest $DOCKER_PACKAGE
+
+    # Login ke GitHub Packages dengan token personal access (PAT)
+    echo $GHCR_PAT | docker login ghcr.io -u novi-adi --password-stdin
+
+    # Mengunggah image ke GitHub Packages
+    docker image push $DOCKER_PACKAGE
+
+else
+    echo "Docker is not installed. Please install Docker before running this script."
+    exit 1
+fi
